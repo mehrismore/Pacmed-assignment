a76f370cafaec6e6e8755c6f235309b2beb8a779
Mehrnoosh Beniss <mehrnoosh.beniss@gmail.com>
2021-08-17 16:06:15 +0200

    WIP: add chart data

--- pacmed-app/client/src/components/LineChart.js
+++ pacmed-app/client/src/components/LineChart.js
@@ -1,40 +1,81 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import axios from "axios";
 
 const LineChart = () => {
-  return (
-    <div>
-      <Line
-        data={{
-          labels: ["10 Aug", "11 Aug", "12 Aug", "13 Aug", "Gisteren", "Vandaag"],
+  const [chartData, setChartData] = useState({});
+  const [xAxisData, setXAxisData] = useState([]);
+  const [yAxisData, setYAxisData] = useState([]);
+
+  const chart = () => {
+    let xAxisData = [];
+    let yAxisData = [];
+    axios
+      .get("../data/chart.json")
+      .then((response) => {
+        console.log(response);
+        for (const dataObj of response.data_for_graph.complete) {
+          xAxisData.push(parseInt(dataObj.data_for_graph.complete[0]));
+          yAxisData.push(parseInt(dataObj.data_for_graph.complete[1]));
+        }
+        setXAxisData([xAxisData]);
+        setYAxisData([yAxisData]);
+        setChartData({
+          labels: ['25 Jan', '26 Jan', '27 Jan','28 Jan', 'Gisteren', 'Vandaag'],
           datasets: [
             {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
+              label: "readmission/ mortality risk",
+              data: {xAxisData},
+              fill: true,
+              backgroundColor: "rgba(75,192,192,0.2)",
+              borderColor: "rgba(75,192,192,1)",
             },
           ],
-        }}
-        width={100}
-        height={100}
-        options={{ maintainAspectRatio: false }}
-      />
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(xAxisData, yAxisData);
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+
+  return (
+    <div className='container'>
+      <h2>Pacmed ICU Chart</h2>
+      <div>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "some text", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true,
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
